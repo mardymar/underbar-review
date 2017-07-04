e3b9c1d79953088035eba374b3e315a117024db0
Hack Reactor Student <student@hackreactor.com>
2017-07-03 19:46:33 -0700

    Added up through filter.

--- src/underbar.js
+++ src/underbar.js
@@ -50,6 +50,17 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+
+    if(Array.isArray(collection)) {
+      for(var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for(var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
+
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -71,6 +82,15 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var results = [];
+
+    _.each(collection, function(val){
+      if(test(val)) {
+        results.push(val);
+      }
+    });
+
+    return results;
   };
 
   // Return all elements of an array that don't pass a truth test.
